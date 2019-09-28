bd90a6fbdc0dded440d228dd0f28f8049ce681bb
Evan Jeffries <ejeffries14@gmail.com>
2019-09-28 07:26:28 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_reader :name, :backed_projects
+  
+  back_project = Backer.new
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(back_project)
+    @backed_projects << back_project
+    project.add_backer(self)
+  end
+  
+end
